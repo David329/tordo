a2ebfde5e1f62d77051346d685676d3c1125c13d
David Silva <davidsilva3290@gmail.com>
2016-07-18 00:05:55 -0500

    mi primer commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Persona
+    {
+        public int id;// { get; set; }
+        public int dameid()
+        {
+            return id;
+        }
+    }
+    class Alumno : Persona
+    {
+        public int Edad { get; set; }//Atributo 1
+        public string Nombre { get; set; }//Atributo 2
+        public string Apellido { get; set; }//Atributo 3
+
+        //Metodo Nombrecompleto
+        public void NombreCompleto()
+        {
+            Console.WriteLine(Nombre + " " + Apellido);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos clase alumno
+            Alumno david1 = new Alumno();
+            david1.Nombre = "David";
+            david1.Apellido = "Silva";
+            david1.Edad = 12;
+            david1.NombreCompleto();
+
+            Alumno david2 = new Alumno();
+
+            Console.ReadLine();
+        }
+    }
+}
